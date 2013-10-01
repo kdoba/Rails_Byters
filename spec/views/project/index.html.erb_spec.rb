0933d7bfd28924f02acfc213b6e5595dd3803c75
kdoba <dbkhoa@khoa.local>
2013-09-30 23:11:18 -0700

    make sure tests work

--- spec/views/project/index.html.erb_spec.rb
+++ spec/views/project/index.html.erb_spec.rb
@@ -2,15 +2,31 @@ require 'spec_helper'
 
 describe "projects/index.html.erb" do
 
-  it "should have a title called 'Projects'" do
+  before do
+    30.times do |n|
+      Project.create!(name: "Project"+n.to_s,
+                      description: "Great projects"+n.to_s)
+    end
+
     visit '/projects'
+  end
+
+  it "should have a title called 'Projects'" do
     expect(page).to have_title('Projects')
   end
 
+  it "should have a page header 'Existing Projects'" do
+    expect(page).to have_content('Existing Projects')
+  end
+
+  it "should have a table with headers for name and description" do
+    expect(page).to have_table('projects-table')
+    expect(page.find('#projects-table thead tr')).to have_content("Name")
+    expect(page.find('#projects-table thead tr')).to have_content("Description")
+  end
+
   it "should have a content from 'Projects'" do
-    visit '/projects'
     30.times do |n|
-      #Projects.get
       expect(page).to have_content('Project' + n.to_s)
     end
   end
