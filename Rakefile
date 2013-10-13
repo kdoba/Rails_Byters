d118ebf3b0f1d02fbfb5c2e588d7fa1fe13ad3ae
yglee <yeongrace.lee@gmail.com>
2013-10-13 16:09:55 -0700

    rdoc api stuff

--- Rakefile
+++ Rakefile
@@ -5,3 +5,14 @@
 require File.expand_path('../config/application', __FILE__)
 
 PET::Application.load_tasks
+
+#Referenced from: http://stackoverflow.com/questions/1202060/how-do-i-generate-rdoc-for-all-of-rails
+RDoc::Task.new :rdoc do |rdoc|
+  rdoc.main = "README.rdoc"
+
+  rdoc.rdoc_files.include("README.rdoc", "doc/*.rdoc", "app/**/*.rb", "lib/*.rb", "config/**/*.rb")
+  #change above to fit needs
+
+  rdoc.title = "App Documentation"
+  rdoc.options << "--all"
+end
--- app/models/lifecycle.rb
+++ app/models/lifecycle.rb
@@ -1,3 +1,13 @@
+#* Specific structure of the process of software development. Lifecycle has one to many Lifecycle Phases.
+#* Fields:
+# ** Name: unique character string that identifies the Lifecycle (required)
+# ** Description: character string that explains the Lifecycle (optional)
+#* Required by:
+#  ** Lifecycle Phase
+#  ** Project
+#  ** Historical Data
+#* Notes: Non editable in PET 1.1
+
 class Lifecycle < ActiveRecord::Base
   has_many :lifecycle_phases
   has_many :projects
