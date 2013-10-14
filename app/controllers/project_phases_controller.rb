f9fb7f9c46adb54c1dfbff3db580d7062311e71a
kdoba <dbkhoa@khoa.local>
2013-10-13 18:36:52 -0700

    add rdoc documentation for ProjectController and ProjectPhaseController

--- app/controllers/project_phases_controller.rb
+++ app/controllers/project_phases_controller.rb
@@ -1,5 +1,16 @@
+# * *Filename*    : project_phases_controller.rb
+# * *Date*        : 09-30-2013
+# * *Team*        : Rails Byters
+# * *Description* : This controller provides an api to query and create project phases in PET
+#
 class ProjectPhasesController < ApplicationController
 
+  # This function accept a project id and returns all project phases that belongs to the given project
+  # in the database.
+  # * *Args*    :
+  #   - <tt>projectId:number</tt> - the id of a project in project table
+  # * *Returns* :
+  #   - an Array of projectPhases that belongs to the given project
   def self.getProjectPhases (projectId)
     projectPhases = ProjectPhase.where :project_id => projectId
     projectPhases.to_a
--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -1,17 +1,37 @@
+# * *Filename*    : projects_controller.rb
+# * *Date*        : 09-30-2013
+# * *Team*        : Rails Byters
+# * *Description* : This controller provides a REST api to query, create, and modify projects in PET
+#
 class ProjectsController < ApplicationController
 
-  #GET /projects
+  # This function joins project table with lifecycle table and returns a list of all projects
+  # with matching lifecycle information
+  # * *Http*  :
+  #   - <tt>GET /projects</tt>
   def index
     @projects = Project.joins(:lifecycle)
   end
 
-  #GET /projects/1
+  # This function returns a project with a given id and its matching lifecycle information
+  # * *Http*  :
+  #   - <tt>GET /projects/1</tt>
   def show
     @project_phases = ProjectPhasesController.getProjectPhases(params[:id])
     @project = Project.joins(:lifecycle).find(params[:id])
   end
 
-  #POST /projects
+  # This function accept a json string as parameters for a new project and attempt to create
+  # a new project in the database.
+  # * *Args*    :
+  #   - <tt>name:string</tt> - the name of the project
+  #   - <tt>description:string</tt> - the description string of the project
+  #   - <tt>lifecycle_id:number</tt> - the id of the project lifecycle
+  # * *Returns* :
+  #   - if successful: redirects to project overview page and flash success message
+  #   - if unsuccessful: redirects to new project form and flash error message
+  # * *Http*    :
+  #   - <tt>POST /projects</tt>
   def create
     @new_project = Project.new(name: params[:project][:name],
                                description: params[:project][:description],
@@ -29,6 +49,9 @@ class ProjectsController < ApplicationController
     end
   end
 
+  # This function gets a list of all lifecycles in lifecycle table and renders the new project html form
+  # * *Http*  :
+  #   - <tt>GET /projects/new</tt>
   def new
     @new_project = Project.new
     @lifecycles  = Lifecycle.all
